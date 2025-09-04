551461587ac0c2781566849cd5d5a2688444f8c4
vinicius-santana04 <viniciusantanagato@gmail.com>
2025-09-04 02:01:10 -0400

    feat: setup database connections

---
+++ app/database/nosql.py
@@ -0,0 +1,19 @@
+# app/database/nosql.py
+from pymongo import MongoClient
+import os
+
+# Carrega as variáveis de ambiente
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGODB_URL = os.getenv("MONGODB_URL")
+
+# Cria o cliente do MongoDB
+client = MongoClient(MONGODB_URL)
+
+# Define o nome do banco de dados a ser usado
+db = client["movie_db"]
+
+# Função para obter o banco de dados
+def get_nosql_db():
+    return db
---
+++ app/database/sql.py
@@ -0,0 +1,31 @@
+# app/database/sql.py
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+# Carrega as variáveis de ambiente do .env
+from dotenv import load_dotenv
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Cria a engine do SQLAlchemy
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {}
+)
+
+# Cria uma SessionLocal que será usada para as transações no banco
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Uma classe base para os modelos declarativos
+Base = declarative_base()
+
+# Função para obter uma sessão de banco de dados
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
